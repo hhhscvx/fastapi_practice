34a135d4a58976dbc38f98e00fd368dc8a4e7219
hhhscvx <hhhscvx@protonmail.com>
2024-09-03 08:27:34 +0500

    Create user relation mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,26 +1,16 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 
-if TYPE_CHECKING:
-    from .user import User
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
 
-
-class Post(Base):
     title: Mapped[str] = mapped_column(String(100))
     body: Mapped[str] = mapped_column(
         type_=Text,
         default="",  # дефолт значение, если создаем через алхимию
         server_default=""  # дефолт значение, даже если создадим через базу
     )
-
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-        # nullable=False
-    )
-
-    user: Mapped[User] = relationship(back_populates="posts") # как в джанго related_name)
